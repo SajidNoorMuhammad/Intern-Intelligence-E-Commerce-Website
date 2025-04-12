0de2bbdb5eadc4d77a4fa02578c556d6d7312f18
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-12 15:39:17 +0500

    update.

--- src/App.jsx
+++ src/App.jsx
@@ -6,16 +6,25 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/All/Home'
 import LoginPage from './pages/auth/login'
 import RegisterPage from './pages/auth/register'
+import Profile from './pages/User/Profile'
+import Header from './components/User/Header'
+import Products from './pages/User/Products'
+import AddProduct from './pages/Admin/AddProducts'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/login' element={<LoginPage />} />
         <Route path='/register' element={<RegisterPage />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/products' element={<Products />} />
+        <Route path='/addproducts' element={<AddProduct />} />
+
       </Routes>
     </BrowserRouter>
   )
