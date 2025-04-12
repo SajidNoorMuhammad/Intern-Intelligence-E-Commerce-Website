8c105acb61d14916f171e9589149f84980d7c855
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-12 11:55:53 +0500

    add constant file.

---
+++ src/constant/constant.js
@@ -0,0 +1,8 @@
+const devUrl = 'https://electric-app-backend.vercel.app/'
+export const BASE_URL = devUrl;
+
+export const AppRoutes = {
+    login: BASE_URL + 'authlogin/login',
+    getinfo: BASE_URL + 'authlogin/getInfo',
+    registers: BASE_URL + 'auth/register'
+}
