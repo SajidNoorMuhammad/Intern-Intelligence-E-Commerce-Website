88b000942fc645af39b829a34c4f74c31f90e72d
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-16 02:15:04 +0500

    update.

--- src/components/Home/FourImage.jsx
+++ src/components/Home/FourImage.jsx
@@ -4,10 +4,10 @@ const FourImage = () => {
     return (
         <div className="w-full px-2">
             <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
-                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty1.png" alt="Beauty 1" />
-                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty2.jpg" alt="Beauty 2" />
-                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty3.jpg" alt="Beauty 3" />
-                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty5.png" alt="Beauty 4" />
+                <img className="w-full rounded-sm object-cover max-sm:w-80" src="/Images/beauty/beauty1.png" alt="Beauty 1" />
+                <img className="w-full rounded-sm object-cover max-sm:w-80" src="/Images/beauty/beauty2.jpg" alt="Beauty 2" />
+                <img className="w-full rounded-sm object-cover max-sm:w-80" src="/Images/beauty/beauty3.jpg" alt="Beauty 3" />
+                <img className="w-full rounded-sm object-cover max-sm:w-80" src="/Images/beauty/beauty5.png" alt="Beauty 4" />
             </div>
         </div>
     );
