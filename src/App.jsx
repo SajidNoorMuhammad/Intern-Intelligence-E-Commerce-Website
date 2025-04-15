34af2464c5e68780b5c35a0214989ff0175d1964
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-15 13:22:53 +0500

    update router.

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,10 @@ import Profile from './pages/User/Profile'
 import Header from './components/User/Header'
 import Products from './pages/User/Products'
 import AddProduct from './pages/Admin/AddProducts'
+import ProductDetail from './pages/User/ProductDetail'
+import Cart from './pages/User/Cart'
+import Order from './pages/User/Order'
+import Wishlist from './pages/User/Wishlist'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -23,7 +27,11 @@ function App() {
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/profile' element={<Profile />} />
         <Route path='/products' element={<Products />} />
+        <Route path='/products/:id' element={<ProductDetail />} />
         <Route path='/addproducts' element={<AddProduct />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/order' element={<Order />} />
+        <Route path='/wishlist' element={<Wishlist />} />
 
       </Routes>
     </BrowserRouter>
