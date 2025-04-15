6b6a30083b20707f71299050e3f2230422e33561
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-15 15:06:38 +0500

    create.

---
+++ src/components/Home/Collection.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const Collection = () => {
+    const images = [
+        "/Images/Collections/men.webp",
+        "/Images/Collections/women.webp",
+        "/Images/Collections/kids.webp",
+        "/Images/Collections/footwear.webp",
+        "/Images/Collections/beauty.webp",
+        "/Images/Collections/home.webp",
+    ];
+
+    return (
+        <div className="py-10 md:px-10 bg-white">
+            <h2 className="text-3xl font-bold mb-4 text-blue-700">Collections</h2>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
+                {images.map((img, idx) => (
+                    <div
+                        key={idx}
+                        className="overflow-hidden rounded-2xl shadow-lg hover:shadow-2xl transition duration-300"
+                    >
+                        <img
+                            src={img}
+                            alt={`Collection ${idx + 1}`}
+                            className="w-full h-70 object-cover hover:scale-105 transition-transform duration-300"
+                        />
+                    </div>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Collection;
