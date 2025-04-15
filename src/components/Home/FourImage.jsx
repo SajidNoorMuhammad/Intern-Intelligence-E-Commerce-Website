8e2404e0e2ff4376a33a4c9c52b4425114529cc1
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-16 02:12:45 +0500

    make responsive.

--- src/components/Home/FourImage.jsx
+++ src/components/Home/FourImage.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 const FourImage = () => {
     return (
         <div className="w-full px-2">
-            <div className="flex gap-6">
-                <img className="w-78 rounded-sm object-cover" src="/Images/beauty/beauty1.png" alt="Beauty 1" />
-                <img className="w-78 rounded-sm object-cover" src="/Images/beauty/beauty2.jpg" alt="Beauty 2" />
-                <img className="w-78 rounded-sm object-cover" src="/Images/beauty/beauty3.jpg" alt="Beauty 3" />
-                <img className="w-78 rounded-sm object-cover" src="/Images/beauty/beauty5.png" alt="Beauty 4" />
+            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-4">
+                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty1.png" alt="Beauty 1" />
+                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty2.jpg" alt="Beauty 2" />
+                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty3.jpg" alt="Beauty 3" />
+                <img className="w-full rounded-sm object-cover max-sm:w-90" src="/Images/beauty/beauty5.png" alt="Beauty 4" />
             </div>
         </div>
     );
