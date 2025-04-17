e7492b64aa6ac15c7227885011be4f46726ee4f8
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-17 13:53:47 +0500

    update.

--- src/pages/auth/login.jsx
+++ src/pages/auth/login.jsx
@@ -5,6 +5,7 @@ import { AuthContext } from '../../context/AuthContext';
 import { useNavigate } from 'react-router';
 import Cookies from 'js-cookie';
 import { Eye, EyeOff } from 'lucide-react'; // You can also use any icon library
+import { Link } from 'react-router-dom';
 
 export default function LoginPage() {
     const [loading, setLoading] = useState(false);
@@ -35,7 +36,7 @@ export default function LoginPage() {
     };
 
     return (
-        <div className="min-h-screen flex items-center justify-center bg-gradient-to-br from-purple-300 via-pink-200 to-yellow-100 p-4">
+        <div className=" max-sm:min-h-0 max-sm:py-12 py-16 flex items-center justify-center bg-gradient-to-br from-purple-300 via-pink-200 to-yellow-100 p-4">
             <div className="bg-white rounded-2xl shadow-lg p-8 w-full max-w-md transition-transform transform hover:scale-105">
                 <h2 className="text-2xl font-bold text-center text-gray-800 mb-6">Login</h2>
                 <form onSubmit={handleLogin} className="space-y-5">
@@ -64,9 +65,9 @@ export default function LoginPage() {
                             {showPassword ? <EyeOff size={20} /> : <Eye size={20} />}
                         </span>
                     </div>
-                    <div className="flex justify-between text-sm text-gray-500">
-                        <span>Don't have an account? <a href="#" className="text-indigo-600 hover:underline">Sign up</a></span>
-                        <a href="#" className="text-indigo-600 hover:underline">Forgot Password?</a>
+                    <div className="flex justify-between max-sm:justify-baseline text-sm text-gray-500 ">
+                        <span>Don't have an account? <Link to={"/register"} className="text-indigo-600 hover:underline">Sign up</Link></span>
+                        <Link to={'/forget'} className="text-indigo-600 hover:underline">Forgot Password?</Link>
                     </div>
                     <button
                         type="submit"
