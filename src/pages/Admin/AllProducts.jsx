f14f5e2e89408689e0337b25917f171aba8f6a02
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-18 15:20:39 +0500

    create.

---
+++ src/pages/Admin/AllProducts.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import AdminHeader from '../../components/Admin/MainHeader/Header'
+
+const AllProducts = () => {
+    return (
+        <div>
+            <AdminHeader/>
+            hello all products page.
+        </div>
+    )
+}
+
+export default AllProducts
