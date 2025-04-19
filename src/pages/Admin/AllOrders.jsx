70e343785b9e791b3375076e0b1b6fca95348a8f
Sajid Noor Muhammad <153657286+SajidNoorMuhammad@users.noreply.github.com>
2025-04-20 01:54:17 +0500

    add all order page.

---
+++ src/pages/Admin/AllOrders.jsx
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import React, { useEffect } from 'react'
+import { AppRoutes } from '../../constant/constant';
+
+const AllOrders = () => {
+    useEffect(() => {
+    }, [])
+
+    return ( 
+        <div>
+
+        </div>
+    )
+}
+
+export default AllOrders
